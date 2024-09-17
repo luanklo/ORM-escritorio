9eec919dd25f8718bbfd00f061c7ebd79352564f
Luan kloh <luanj.kloh@hotmail.com>
2024-09-17 16:29:55 -0300

    corrigindo erros

--- run.py
+++ run.py
@@ -1,16 +1,12 @@
 from infra.Controllers.PessoaController import PessoaController
 
-#repo = AtorRepository()
-#repo.inser("algum filme", "comedia", 2101)
-#repo.delete("algum filme")
-#repo.update("Forest Gump", 2000)
-#data = repo.select()
-#for x in data: print(x)
 
-# repo2 = FilmeRepository()
-# data2 = repo2.select()
-# print(data2)
 
 repo = PessoaController()
-data = repo.select()
+
+# repo.insert("luan", "123.123.123-65", "12 1234 1234")
+# repo.insert("pedro", "123.123.123-64", "12 1234 1233")
+# repo.insert("jose", "123.123.123-63", "12 1234 1232")
+
+data = repo.select(nome="luan")
 for x in data: print(x)
