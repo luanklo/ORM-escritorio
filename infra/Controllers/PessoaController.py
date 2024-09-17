d0b575ceb20fa4b30962f59a5f7b591f69b5f66c
Luan kloh <luanj.kloh@hotmail.com>
2024-09-17 17:41:14 -0300

    correção de erro

--- infra/Controllers/PessoaController.py
+++ infra/Controllers/PessoaController.py
@@ -2,10 +2,11 @@ from infra.Configs.connection import BDConnectionHandler
 from infra.Models.Pessoa import Pessoa
 
 class PessoaController:
-    def select(self, nome=None, cpf_cnpj=None, numero=None):
+    def select(self,id=None, nome=None, cpf_cnpj=None, numero=None):
         with BDConnectionHandler() as db:
             query = db.session.query(Pessoa)
             
+            if id:       query = query.filter(Pessoa.id==id)
             if nome:     query = query.filter(Pessoa.nome==nome)
             if cpf_cnpj: query = query.filter(Pessoa.cpf_cnpj==cpf_cnpj)
             if numero:   query = query.filter(Pessoa.numero==numero)
@@ -19,21 +20,23 @@ class PessoaController:
             db.session.add(data_insert)
             db.session.commit()
     
-    def delete(self, nome=None, cpf_cnpj=None, numero=None):
+    def delete(self,id=None, nome=None, cpf_cnpj=None, numero=None):
         with BDConnectionHandler() as db:
             query = db.session.query(Pessoa)
 
-            if nome: query.filter(Pessoa.nome==nome)
-            if cpf_cnpj: query.filter(Pessoa.cpf_cnpj==cpf_cnpj)
-            if numero: query.filter(Pessoa.numero==numero)
+            if id:       query = query.filter(Pessoa.id==id)
+            if nome:     query = query.filter(Pessoa.nome==nome)
+            if cpf_cnpj: query = query.filter(Pessoa.cpf_cnpj==cpf_cnpj)
+            if numero:   query = query.filter(Pessoa.numero==numero)
 
             query.delete()
             db.session.commit()
     
-    def update(filtro_nome=None, filtro_cpf_cnpj=None, filtro_numero=None, **updates):
+    def update(filtro_id=None, filtro_nome=None, filtro_cpf_cnpj=None, filtro_numero=None, **updates):
         with BDConnectionHandler() as db:
             query = db.query(Pessoa)
             
+            if filtro_id:       query = query.filter(Pessoa.id==filtro_id)
             if filtro_nome:     query = query.filter(Pessoa.nome==filtro_nome)
             if filtro_cpf_cnpj: query = query.filter(Pessoa.cpf_cnpj==filtro_cpf_cnpj)
             if filtro_numero:   query = query.filter(Pessoa.numero==filtro_numero)
--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 from infra.Controllers.PessoaController import PessoaController
 
-
-
 repo = PessoaController()
 
 # repo.insert("luan", "123.123.123-65", "12 1234 1234")
 # repo.insert("pedro", "123.123.123-64", "12 1234 1233")
 # repo.insert("jose", "123.123.123-63", "12 1234 1232")
 
+repo.select(filtro_id=9)
 data = repo.select(nome="luan")
 for x in data: print(x)
