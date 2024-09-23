c52bf850d3a0c40b354a63875bacc1c6c26f55c9
Luan kloh <luanj.kloh@hotmail.com>
2024-09-23 17:35:41 -0300

    add audiencia model

---
+++ infra/Models/Audiencia.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, Time, Date
+from infra.Configs.base import base
+
+class Audiencia(base):
+    __tablename__ = "audiencia"
+    #id, hora, dia, tipo, status, link, senha, processo_id
+
+    id = Column(Integer, primary_key=True)
+    hora = Column(Time, nullable=False)
+    dia = Column(Date, nullable=False)
+    tipo = Column(String, nullable=False)
+    status = Column(String, nullable=False)
+    link = Column(String, nullable=True)
+    senha = Column(String, nullable=True)
+    processo_id = Column(Integer, ForeignKey("processo.id"), nullable=False)
+
+    def __repr__(self):
+        return f"Audiencia [id={self.id}, hora={self.hora}, dia={self.dia}, tipo={self.tipo}, status={self.status}, link={self.link}, senha={self.senha}, processo_id={self.processo_id}]"
