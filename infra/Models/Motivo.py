bc14ee936285e4464198532b3b2a68307d10f77e
Luan kloh <luanj.kloh@hotmail.com>
2024-09-23 16:59:35 -0300

    add  relação entre motivo e processo

--- infra/Models/Motivo.py
+++ infra/Models/Motivo.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from infra.Configs.base import base
+from sqlalchemy.orm import relationship
 
 class Motivo(base):
     __tablename__ = "motivo"
@@ -7,6 +8,9 @@ class Motivo(base):
 
     id = Column(Integer, primary_key=True)
     nome = Column(String, nullable=False)
+
+    motivos = relationship("Processo", secondary="processo_motivo", backref="motivo")
+
 
     def __repr__(self):
         return f"Processo [id={self.id}, nome={self.nome}]"
--- infra/Models/Processo.py
+++ infra/Models/Processo.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from infra.Configs.base import base
+from sqlalchemy.orm import relationship
 
 class Processo(base):
     __tablename__ = "processo"
@@ -12,6 +13,8 @@ class Processo(base):
     classe = Column(String, nullable=False)
     link = Column(String, nullable=False)
     profissao = Column(String, nullable=True)
+
+    motivos = relationship("Motivo", secondary="processo_motivo", backref="processo")
 
     def __repr__(self):
         return f"Processo [id={self.id}, numero={self.numero}, valor={self.valor}, vara={self.vara}, classe={self.classe}, link={self.link}, profissao={self.profissao}]"
