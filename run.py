9fdbdddb7505cc8d715f7add0426536ad1eda2c4
Luan kloh <luanj.kloh@hotmail.com>
2024-09-23 15:27:53 -0300

    teste

--- run.py
+++ run.py
@@ -3,10 +3,6 @@ from infra.Controllers.AdvogadoController import AdvogadoController
 
 repo = AdvogadoController()
 
-# repo.insert("luan", "123.123.123-65", "12 1234 1234")
-# repo.insert("pedro", "123.123.123-64", "12 1234 1233")
-# repo.insert("jose", "123.123.123-63", "12 1234 1232")
-
 adv = repo.select(id=4)
 repo.update(adv, nome="teste")
 
